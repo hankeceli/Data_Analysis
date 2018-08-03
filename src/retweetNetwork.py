4435733eff4d0fd9e587831fed6cbe765535adc5
Hamza Haruna MOHAMMED <hamzasharoon@gmail.com>
2018-08-03 11:37:21 +0300

    Update retweetNetwork.py
    Adding more comments.......

--- src/retweetNetwork.py
+++ src/retweetNetwork.py
@@ -1,4 +1,4 @@
-# Databricks notebook source# Databricks notebook source
+# Databricks notebook source
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug  1 10:53:14 2018
@@ -13,6 +13,7 @@ from operator import add
 from pyspark.sql import SparkSession
 os.environ['PYSPARK_PYTHON'] = '/usr/bin/python3'
 
+#spark config
 conf = SparkConf()
 sc = SparkContext.getOrCreate()
 
@@ -26,7 +27,7 @@ def mapFunc(line):
 def redFunc(line1, line2):
 	return line2
 
-#reading a single file for now
+ #reading a single file for now
 input = sc.textFile('/FileStore/tables/20180624000034.txt')
 
 #mapReduce for finding unique tweets {tweet: id}
@@ -36,10 +37,8 @@ print(":::::::::::::::TASK-2(Result)::::::::::::: \n")
 
 #print("The unique tweet-terms are: \n")
 #printing pairs (unique_tweet and its ID)
-
-#Iterating through ouput list
 for(id, unique_tweet) in output:
-   # print("%s: %d" % (unique_tweet, id))
+    print("%s: %d" % (unique_tweet, id))
     
     #print("\n")
     #print("\n")
@@ -48,10 +47,19 @@ for(id, unique_tweet) in output:
 
     
     print("The output of (user1, user2: retweet_count): ")
-    response_item = literal_eval(json.dumps(unique_tweet, ensure_ascii=False).encode('utf8'))
+    
+    #converting unicode of unique tweets to pure json unique_tweets
+    jsonUniqueTweet = json.dumps(unique_tweet, ensure_ascii=False)
+    
+    #converting to dict
+    response_item = literal_eval(jsonUniqueTweet.encode('utf8'))
     index = json.loads(response_item)
     
-    try: #if key exist
+    # =============================================================================
+    # Indexing through index dictionary to fetch the user1 and user2 with the number of retweets
+    # Exception handling: when a retweet key  doesn't exist in a particular tweet.
+    # =============================================================================
+    try:
       print("\t %d, %d, %d " % (index['id'], 
                            index['retweeted_status']['id'],
                            index['retweeted_status']['retweet_count']))

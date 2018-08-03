3f0a5124c6822c1eb31490fd7ed5a52261d8d2ab
Hamza Haruna MOHAMMED <hamzasharoon@gmail.com>
2018-08-03 11:26:22 +0300

    Update retweetNetwork.py
    Exception hanglling changes: if retweet_status or retweet_count  key  doesn't exist.

--- src/retweetNetwork.py
+++ src/retweetNetwork.py
@@ -1,4 +1,4 @@
-# Databricks notebook source
+# Databricks notebook source# Databricks notebook source
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug  1 10:53:14 2018
@@ -26,27 +26,37 @@ def mapFunc(line):
 def redFunc(line1, line2):
 	return line2
 
+#reading a single file for now
 input = sc.textFile('/FileStore/tables/20180624000034.txt')
 
-output = input.map(lambda x: (mapFunc(x),x)).reduceByKey(lambda x,y: redFunc(x,y)).sortByKey(ascending=False).take(1)
+#mapReduce for finding unique tweets {tweet: id}
+output = input.map(lambda x: (mapFunc(x),x)).reduceByKey(lambda x,y: redFunc(x,y)).sortByKey(ascending=False).collect()
 
+print(":::::::::::::::TASK-2(Result)::::::::::::: \n")
 
-print("The unique tweet-terms are: \n")
+#print("The unique tweet-terms are: \n")
 #printing pairs (unique_tweet and its ID)
+
+#Iterating through ouput list
 for(id, unique_tweet) in output:
-    print("%s: %d" % (unique_tweet, id))
+   # print("%s: %d" % (unique_tweet, id))
     
-    print("\n")
-    print("\n")
-    print(":::::::::::::::user1, user2: retweet_count(output_Result)::::::::::::: \n")
+    #print("\n")
+    #print("\n")
+    #print(":::::::::::::::TASK-2(Result)::::::::::::: \n")
     
 
     
-print("The output of (user1, user2: retweet_count): ")
-response_item = literal_eval(json.dumps(unique_tweet, ensure_ascii=False).encode('utf8'))
-index = json.loads(response_item)
-
-
-print("%d, %d, %d " % (index['id'], 
+    print("The output of (user1, user2: retweet_count): ")
+    response_item = literal_eval(json.dumps(unique_tweet, ensure_ascii=False).encode('utf8'))
+    index = json.loads(response_item)
+    
+    try: #if key exist
+      print("\t %d, %d, %d " % (index['id'], 
                            index['retweeted_status']['id'],
                            index['retweeted_status']['retweet_count']))
+
+      print("\n")
+    except: #Key doesn't exist
+      print("\t Retweet doesn't exist")
+      print("\n")

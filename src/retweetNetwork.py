1855d5ef6ca95ea3c88981311a26f2312e6c2b2e
Hamza Haruna MOHAMMED <hamzasharoon@gmail.com>
2018-08-27 12:28:48 +0300

    Update retweetNetwork.py

--- src/retweetNetwork.py
+++ src/retweetNetwork.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Aug  12 10:53:14 2018
-
 @author: hamza.mohammed
 """
 from ast import literal_eval
@@ -20,35 +19,36 @@ def mapTweet(line):
     #============================================================================================
     # Exception handling: when a retweet key  doesn't exist in a particular tweet.
     # ===========================================================================================
+    
+  #converting unicode of unique tweets to pure json unique_tweets
+  jsonUniqueTweet = json.dumps(line, ensure_ascii=False)
+  
+  #dict
+  response_item = literal_eval(jsonUniqueTweet.encode('utf8'))
+  
+  
   try:
-    #converting unicode of unique tweets to pure json unique_tweets
-    jsonUniqueTweet = json.dumps(line, ensure_ascii=False)
-    
-    #dict
-    response_item = ast.literal_eval(jsonUniqueTweet.encode('utf8'))
     index = json.loads(response_item)
-    
     # ===========================================================================================
     #Fetch the user1 and user2 with the number of retweets
     #Making (User1, User2) as Key, while retweetCount as the Value
     # ===========================================================================================
-    user1 = index['user']['id_str']
-    user2 = index['retweeted_status']['user']['id_str']
-    retweetList.append([user1+ " , "+ user2, 1])
+    retweetList.append([index.get('user').get('id_str')+ " , "+ index.get('retweeted_status').get('user').get('id_str'), 1])
     return retweetList
   except:
-    return retweetList
+     return retweetList
+  return retweetList
 
 
 
-
+#Large File (20180624234201)
 file = sc.textFile('/FileStore/tables/20180624000034')
 mapTweets = file.flatMap(lambda line: mapTweet(line))
 reducePairUserRetweets = mapTweets.reduceByKey(lambda a,b:a+b).collect()
 
-#print(reducePairUserRetweets)
+print(reducePairUserRetweets)
 #Saving to directory
-reducePairUserRetweets.saveAsTextFile('/FileStore/tables/2ndVersRetweetNetwork')
+#reducePairUserRetweets.saveAsTextFile('/FileStore/tables/2ndVersRetweetNetwork')
 
 
 #Checking the content of the save file
